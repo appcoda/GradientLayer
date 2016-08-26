1ed2894548bb412de60a3e3b2449c4797260e3d0
nicolas <n.desormiere@kreactive.com>
2016-08-26 11:37:39 +0200

    support iOS 10

--- GradientLayer/ViewController.swift
+++ GradientLayer/ViewController.swift
@@ -33,14 +33,14 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         createColorSets()
-        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.handleTapGesture(_:)))
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.handleTapGesture(gestureRecognizer:)))
         self.view.addGestureRecognizer(tapGestureRecognizer)
         
-        let twoFingerTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.handleTwoFingerTapGesture(_:)))
+        let twoFingerTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.handleTwoFingerTapGesture(gestureRecognizer:)))
         twoFingerTapGestureRecognizer.numberOfTouchesRequired = 2
         self.view.addGestureRecognizer(twoFingerTapGestureRecognizer)
         
-        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(ViewController.handlePanGestureRecognizer(_:)))
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(ViewController.handlePanGestureRecognizer(gestureRecognizer:)))
         self.view.addGestureRecognizer(panGestureRecognizer)
     }
 
@@ -50,7 +50,7 @@ class ViewController: UIViewController {
     }
 
 
-    override func viewWillAppear(animated: Bool) {
+    override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         createGradientLayer()
     }
@@ -63,16 +63,16 @@ class ViewController: UIViewController {
         gradientLayer.colors = colorSets[currentColorSet]
         gradientLayer.locations = [0.0, 0.35]
         
-        gradientLayer.startPoint = CGPointMake(0.0, 0.5)
-        gradientLayer.endPoint = CGPointMake(1.0, 0.5)
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.5)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.5)
         
         self.view.layer.addSublayer(gradientLayer)
     }
     
     func createColorSets() {
-        colorSets.append([UIColor.redColor().CGColor, UIColor.yellowColor().CGColor])
-        colorSets.append([UIColor.greenColor().CGColor, UIColor.magentaColor().CGColor])
-        colorSets.append([UIColor.grayColor().CGColor, UIColor.lightGrayColor().CGColor])
+        colorSets.append([UIColor.red.cgColor, UIColor.yellow.cgColor])
+        colorSets.append([UIColor.green.cgColor, UIColor.magenta.cgColor])
+        colorSets.append([UIColor.gray.cgColor, UIColor.lightGray.cgColor])
         
         currentColorSet = 0
     }
@@ -89,15 +89,12 @@ class ViewController: UIViewController {
         colorChangeAnimation.duration = 2.0
         colorChangeAnimation.toValue = colorSets[currentColorSet]
         colorChangeAnimation.fillMode = kCAFillModeForwards
-        colorChangeAnimation.removedOnCompletion = false
+        colorChangeAnimation.isRemovedOnCompletion = false
         
-        // Add this line to make the ViewController class the delegate of the animation object.
-        colorChangeAnimation.delegate = self
-        
-        gradientLayer.addAnimation(colorChangeAnimation, forKey: "colorChange")
+        gradientLayer.add(colorChangeAnimation, forKey: "colorChange")
     }
     
-    override func animationDidStop(anim: CAAnimation, finished flag: Bool) {
+    func animationDidStop(anim: CAAnimation, finished flag: Bool) {
         if flag {
             gradientLayer.colors = colorSets[currentColorSet]
         }
@@ -107,15 +104,15 @@ class ViewController: UIViewController {
         let secondColorLocation = arc4random_uniform(100)
         let firstColorLocation = arc4random_uniform(secondColorLocation - 1)
         
-        gradientLayer.locations = [NSNumber(double: Double(firstColorLocation)/100.0), NSNumber(double: Double(secondColorLocation)/100.0)]
+        gradientLayer.locations = [NSNumber(value: Double(firstColorLocation)/100.0), NSNumber(value: Double(secondColorLocation)/100.0)]
         
         print(gradientLayer.locations!)
     }
     
     func handlePanGestureRecognizer(gestureRecognizer: UIPanGestureRecognizer) {
-        let velocity = gestureRecognizer.velocityInView(self.view)
+        let velocity = gestureRecognizer.velocity(in: self.view)
         
-        if gestureRecognizer.state == UIGestureRecognizerState.Changed {
+        if gestureRecognizer.state == UIGestureRecognizerState.changed {
             if velocity.x > 300.0 {
                 // In this case the direction is generally towards Right.
                 // Below are specific cases regarding the vertical movement of the gesture.
@@ -167,7 +164,7 @@ class ViewController: UIViewController {
                 }
             }
         }
-        else if gestureRecognizer.state == UIGestureRecognizerState.Ended {
+        else if gestureRecognizer.state == UIGestureRecognizerState.ended {
             changeGradientDirection()
         }
     }
@@ -176,36 +173,36 @@ class ViewController: UIViewController {
         if panDirection != nil {
             switch panDirection.rawValue {
             case PanDirections.Right.rawValue:
-                gradientLayer.startPoint = CGPointMake(0.0, 0.5)
-                gradientLayer.endPoint = CGPointMake(1.0, 0.5)
+                gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.5)
+                gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.5)
                 
             case PanDirections.Left.rawValue:
-                gradientLayer.startPoint = CGPointMake(1.0, 0.5)
-                gradientLayer.endPoint = CGPointMake(0.0, 0.5)
+                gradientLayer.startPoint = CGPoint(x: 1.0, y: 0.5)
+                gradientLayer.endPoint = CGPoint(x: 0.0, y: 0.5)
                 
             case PanDirections.Bottom.rawValue:
-                gradientLayer.startPoint = CGPointMake(0.5, 0.0)
-                gradientLayer.endPoint = CGPointMake(0.5, 1.0)
+                gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.0)
+                gradientLayer.endPoint = CGPoint(x: 0.5, y: 1.0)
                 
             case PanDirections.Top.rawValue:
-                gradientLayer.startPoint = CGPointMake(0.5, 1.0)
-                gradientLayer.endPoint = CGPointMake(0.5, 0.0)
+                gradientLayer.startPoint = CGPoint(x: 0.5, y: 1.0)
+                gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.0)
                 
             case PanDirections.TopLeftToBottomRight.rawValue:
-                gradientLayer.startPoint = CGPointMake(0.0, 0.0)
-                gradientLayer.endPoint = CGPointMake(1.0, 1.0)
+                gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
+                gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
                 
             case PanDirections.TopRightToBottomLeft.rawValue:
-                gradientLayer.startPoint = CGPointMake(1.0, 0.0)
-                gradientLayer.endPoint = CGPointMake(0.0, 1.0)
+                gradientLayer.startPoint = CGPoint(x: 1.0, y: 0.0)
+                gradientLayer.endPoint = CGPoint(x: 0.0, y: 1.0)
                 
             case PanDirections.BottomLeftToTopRight.rawValue:
-                gradientLayer.startPoint = CGPointMake(0.0, 1.0)
-                gradientLayer.endPoint = CGPointMake(1.0, 0.0)
+                gradientLayer.startPoint = CGPoint(x: 0.0, y: 1.0)
+                gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.0)
                 
             default:
-                gradientLayer.startPoint = CGPointMake(1.0, 1.0)
-                gradientLayer.endPoint = CGPointMake(0.0, 0.0)
+                gradientLayer.startPoint = CGPoint(x: 1.0, y: 1.0)
+                gradientLayer.endPoint = CGPoint(x: 0.0, y: 0.0)
             }
         }
     }
